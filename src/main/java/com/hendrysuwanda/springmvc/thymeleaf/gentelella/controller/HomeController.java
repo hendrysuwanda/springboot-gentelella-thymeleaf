97d1abf3f4e7b659e1e00aedf585f31a9dcc5793
hendrysuwanda <suwanda.hendry@gmail.com>
2017-11-09 21:47:21 +0700

    first commit

---
+++ src/main/java/com/hendrysuwanda/springmvc/thymeleaf/gentelella/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.hendrysuwanda.springmvc.thymeleaf.gentelella.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/index1")
+    public String index1(){
+        return "index1";
+    }
+
+    @RequestMapping("/index2")
+    public String index2(){
+        return "index2";
+    }
+
+    @RequestMapping("/index3")
+    public String index3(){
+        return "index3";
+    }
+
+}
